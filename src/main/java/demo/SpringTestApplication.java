541c1e4cd5b5593474e6673da8a2431ea2f09f65
Martin Cvetkovski <martin@Martins-MacBook-Pro.local>
2015-09-20 18:27:28 +0200

    Initial commit

---
+++ src/main/java/demo/SpringTestApplication.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Zdravo Svetu");
+        return model;
+    }
+}
